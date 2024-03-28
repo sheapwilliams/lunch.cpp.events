1836cce38c6418e147a8619e28da8e894bb8de5b
Kevin B. Carpenter <kevin.carpenter@live.com>
2024-03-28 06:14:51 -0700

    Update to read .env file.

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 import json
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SavingAspenLunchesIn2024'
